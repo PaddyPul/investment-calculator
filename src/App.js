45d300dcb3b3853fc5bb8663da6aef9dc4233505
Bortolotti <davibortolotti@gmail.com>
2018-05-08 17:22:18 -0300

    create form for getting data from user

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import InvestmentChoices from './containers/investmentChoices';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        App started.
+        <InvestmentChoices />
       </div>
     );
   }
